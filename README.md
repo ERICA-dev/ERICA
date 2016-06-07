bac728438dc76b040350c50a47679aec335ed578
Edvard <edvardlindelof@gmail.com>
2016-06-07 23:53:51 +0200

    gjorde simpeltsimpelt python-test och fyllde i readme om ett

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ short intro
 ## How to run
 create a file called config.json in the same place as config_template.json
 
+## Testing
+To run python tests, move to the python directory (something/ERICA/python) and type <code>python -m unittest discover</code>.
+
 ## Modules
 #### diff_history
 #### diff_interpreter
---
+++ python/config/test.py
@@ -0,0 +1,15 @@
+import unittest
+import types
+import Config
+
+
+class TestConfigGetType(unittest.TestCase):
+
+    # test names must begin with test
+    def test_get(self):
+        bus_ip = Config.get("bus_ip")
+        print bus_ip
+        self.assertTrue(type(bus_ip) in types.StringTypes)
+
+if __name__ == '__main__':
+    unittest.main()
