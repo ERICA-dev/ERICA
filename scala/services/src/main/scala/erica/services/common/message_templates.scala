0e0b22e7bf6ab79b38c179f579cb61895ce39d95
marptt <laxenmeflaxen+git@gmail.com>
2016-06-08 00:44:16 +0200

    Interpreter can now listen to diffs and also unpack them

---
+++ scala/services/src/main/scala/erica/services/common/message_templates.scala
@@ -0,0 +1,40 @@
+package erica.services.common
+
+import com.github.nscala_time.time.Imports._
+import org.json4s.JsonAST.JValue
+import org.json4s._
+
+/**
+  * Created by marp on 2016-06-07.
+  */
+
+case class EricaEvent(Title: String,
+                      Timestamp: Long,
+                      SubjectId: Long,
+                      EventId: Long,
+                      Data: JValue
+                     )
+
+case class ElvisPatient(CareContactId: Int,
+                        CareContactRegistrationTime: DateTime,
+                        DepartmentComment: String,
+                        Events: List[ElvisEvent],
+                        Location: String,
+                        PatientId: Int,
+                        ReasonForVisit: String,
+                        Team: String,
+                        VisitId: Int,
+                        VisitRegistrationTime: DateTime)
+
+case class ElvisEvent(CareEventId: Int,
+                      Category: String,
+                      End: DateTime,
+                      Start: DateTime,
+                      Title: String,
+                      Type: String,
+                      Value: String,
+                      VisitId: Int)
+
+case class PatientDiff(updates: Map[String, JValue], newEvents: List[ElvisEvent], removedEvents: List[ElvisEvent])
+case class RemovedPatient(timestamp: DateTime, patient: ElvisPatient)
+case class NewPatient(timestamp: DateTime, patient: ElvisPatient)
--- scala/services/src/main/scala/erica/services/diff_interpreter/InterpreterMain.scala
+++ scala/services/src/main/scala/erica/services/diff_interpreter/InterpreterMain.scala
@@ -1,7 +1,12 @@
 package erica.services.diff_interpreter
 
-import erica.bus_apis.AMQSubscriber
-import erica.bus_apis.AMQPublisher
+import erica.bus_api.AMQSubscriber
+import erica.bus_api.AMQPublisher
+import erica.services.common._
+import org.joda.time.DateTime
+import org.json4s._
+import org.json4s.native.JsonMethods._
+
 /**
   * Created by marp on 2016-06-06.
   */
@@ -10,6 +15,8 @@ object InterpreterMain extends App {
 }
 
 class Interpreter {
+  implicit val formats = org.json4s.DefaultFormats ++ org.json4s.ext.JodaTimeSerializers.all // json4s needs this for something
+
   val amqSubscriber = new AMQSubscriber
   val amqPublisher = new AMQPublisher
 
@@ -19,6 +26,42 @@ class Interpreter {
     received(mess))
 
   def received(mess:String): Unit = {
-    println("Interpreter received:" + mess)
+    val json:JValue = parse(mess)
+    val messageType = json \ "isa"
+    val messageData = json \ "data"
+
+    println("\n\nInterpreter received:" + mess)
+    println("message type: "+messageType+ "\n")
+
+    messageType match {
+      case JString("newLoad") => newReceived(messageData) // in production these patients should be ignored due to data incompleteness
+      case JString("new")     => newReceived(messageData)
+      case JString("diff")    => diffReceived(messageData)
+      case JString("removed") => removedReceived(messageData)
+      case _ => throw new IllegalArgumentException
+    }
+  }
+
+  def newReceived(mess:JValue): Unit = {
+    println(mess.extract[NewPatient])
+  }
+
+  def diffReceived(mess:JValue): Unit = {
+    println(mess.extract[PatientDiff])
+  }
+
+  def removedReceived(mess:JValue): Unit = {
+    println(mess.extract[RemovedPatient])
+  }
+
+  /**
+    * Converts a DateTime to the more agreeable epoch format
+    */
+  def getEpoch(timestamp:DateTime): Long = {
+    5
+  }
+
+  def publishEvents(events: List[EricaEvent]): Unit = {
+    // foreach event publish it
   }
 }
