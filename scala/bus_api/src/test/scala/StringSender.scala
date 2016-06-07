d0b44f36f243a85706f33198a1ac24a5954f273a
Edvard <edvardlindelof@gmail.com>
2016-06-07 17:41:01 +0200

    bytte Main i bus_api mot ett test

---
+++ scala/bus_api/src/test/scala/StringSender.scala
@@ -0,0 +1,19 @@
+import org.scalatest.FlatSpec
+import erica.bus_api.{AMQPublisher, AMQSubscriber}
+
+class StringSender extends FlatSpec {
+  "A sent string" should "come back the same" in {
+    val str = "heeeeejdaer"
+    var returnedStr = ""
+    val amqSubscriber = new AMQSubscriber
+    val amqPublisher = new AMQPublisher
+    amqSubscriber.subscribe("testTopik", (mess:String) => {
+        returnedStr = mess
+    })
+    // TODO ugly delays cause of conc issue, see amqP and amqS files
+    Thread.sleep(2000)
+    amqPublisher.publish("testTopik", str)
+    Thread.sleep(2000)
+    assert(returnedStr === str)
+  }
+}
